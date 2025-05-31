3db3ac5f76bb2725c9c8baa824c321d4d71a697a
Sumair Shaikh <shaikhsumair01@gmail.com>
2025-05-31 23:20:39 +0530

    Added form styling

--- ToDo_list/Components/App.jsx
+++ ToDo_list/Components/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
+import Header from './Header'
 
 
 
 function App() {
  return(
-  <>
-  <h1>Hello</h1>
-  </>
+  <div className='content'>
+  <Header/>
+  <form className='form-input'>
+    <input type="text" className="form-text"></input>
+    <button className='submit-btn'><i class="fa-solid fa-arrow-right icon"></i></button>
+  </form>
+  </div>
  )
 }
 
---
+++ ToDo_list/Components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <>
+        <h1 className="Header"> To Do list</h1>
+        </>
+    )
+}
